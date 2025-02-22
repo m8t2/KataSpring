b0b44145c887c5b4fc6f515f42f5acf6a00e3472
m8t2 <puck.xi@gmail.com>
2025-02-22 19:42:37 +0800

    Version 1.0

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,21 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        boolean isSingleton = bean == bean2;
+        System.out.println(isSingleton);
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        isSingleton = cat == cat2;
+        System.out.println(isSingleton);
+
         System.out.println(bean.getMessage());
     }
 }
